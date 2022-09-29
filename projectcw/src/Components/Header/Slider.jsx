55bee8a3fae053d7af01bd9448b3a5c025c32844
sohilweb20 <sohailkhanhatewa@gmail.com>
2022-09-29 22:20:00 +0530

    add my day3 Work

---
+++ projectcw/src/Components/Header/Slider.jsx
@@ -0,0 +1,43 @@
+import './style.css'
+import {
+    Drawer,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    Button,
+    useDisclosure
+  } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function DrawerExample() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
+  
+    return (
+      <>
+        <p  className='Buutoncart'
+        ref={btnRef}  onClick={onOpen}>
+        Cart
+        </p>
+        <Drawer
+          isOpen={isOpen}
+          placement='right'
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>CART</DrawerHeader>
+             <h2 className='card1'>Your Card Is Empty</h2> 
+              <Button className='card2'><Link to="/cart">START SHOPPING</Link></Button>
+          </DrawerContent>
+        </Drawer>
+      </>
+    )
+  }
+  export default DrawerExample
