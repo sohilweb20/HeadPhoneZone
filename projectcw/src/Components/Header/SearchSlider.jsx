b4928fab25e6fb23283b797d2f47d10d9b1fb3f0
sohilweb20 <sohailkhanhatewa@gmail.com>
2022-09-30 21:23:59 +0530

    add my day4 work

---
+++ projectcw/src/Components/Header/SearchSlider.jsx
@@ -0,0 +1,31 @@
+import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Input, useDisclosure } from "@chakra-ui/react"
+import React from "react"
+
+
+function DrawerExample2() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
+  
+    return (
+      <>
+        <p ref={btnRef} colorScheme='teal' onClick={onOpen}>
+          Search
+        </p>
+        <Drawer
+          isOpen={isOpen}
+          placement='right'
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>
+            <Input  w="90%" placeholder=' What are you Looking for ...' />
+            </DrawerHeader>
+          </DrawerContent>
+        </Drawer>
+      </>
+    )
+  }
+  export default DrawerExample2
--- projectcw/src/Routing.jsx
+++ projectcw/src/Routing.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import Cart from './Pages/Cart'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
+import Signup from './Pages/Signup'
 
 const Routing = ()=>{
     return   <Routes>
     <Route   path="/login" element={<Login />} />
     <Route  path="/"  element={<Home />} />
     <Route path="/cart" element={<Cart/>}/>
+    <Route path='/Signup' element={<Signup/>}/>
   </Routes>
 }
 
