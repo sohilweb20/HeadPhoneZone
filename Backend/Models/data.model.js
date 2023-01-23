cc469f73b2f5020de290c1a9df5d6abe4de337c8
sohilweb20 <sohailkhanhatewa@gmail.com>
2023-01-23 12:47:59 +0530

    finally fixed login

--- Backend/Models/data.model.js
+++ Backend/Models/data.model.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 const headSChema = mongoose.Schema({
+  id: { type: Number, required: true },
   Title: { type: String, required: true },
-  Price: { type: Number, required: true },
-  Review: { type: String, required: true },
-  Detail: { type: String, required: true },
-  Color: { type: String, required: true },
+  Heading: { type: Number, required: true },
+  Reviews: { type: String, required: true },
+  Brand: { type: String, required: true },
+  Price: { type: String, required: true },
+  Colour: { type: String, required: true },
+  RegularPrice: { type: String, required: true },
+  Details: { type: String, required: true },
   Poster: { type: String, required: true },
 });
 
--- Backend/index.js
+++ Backend/index.js
@@ -29,58 +29,54 @@ app.get("/about", (req, res) => {
 });
 
 //signup here
-// app.post("/Signup", async (req, res) => {
-//   const { firstName, lastName, email, password } = req.body;
-//   const userPresent = await Authmodel.find({ email });
-
-//   if (userPresent?.email) {
-//     res.send("Try loggin in, user is already exist");
-//   } else {
-//     try {
-//       bcrypt.hash(password, 5, async function (err, hash) {
-//         const user = new Authmodel({
-//           firstName,
-//           lastName,
-//           email,
-//           password: hash,
-//         });
-//         await user.save();
-//         res.send({
-//           message: "Sign up successfull",
-//           status: "Ok",
-//         });
-//       });
-//     } catch (err) {
-//       console.log(err);
-//       res.send({
-//         ERR: "Something went wrong in signup the data . please try again later",
-//       });
-//     }
-//   }
-// });
-
-// //login here
-
-// app.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     const user = await Authmodel.find({ email });
-//     if (user.length > 0) {
-//       const hashed_password = user[0].password;
-//       bcrypt.compare(password, hashed_password, function (err, result) {
-//         if (result) {
-//           const token = jwt.sign({ userID: user[0]._id }, "hush");
-//           res.send({ msg: "Login successfull", token: token });
-//         } else {
-//           res.send("Login failed");
-//         }
-//       });
-//     }
-//   } catch {
-//     res.send("Something went wrong, please try again later");
-//   }
-// });
-
+app.post("/Signup", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  const userPresent = await Authmodel.find({ email });
+  if (userPresent?.email) {
+    res.send("Try loggin in, user is already exist");
+  } else {
+    try {
+      bcrypt.hash(password, 5, async function (err, hash) {
+        const user = new Authmodel({
+          firstName,
+          lastName,
+          email,
+          password: hash,
+        });
+        await user.save();
+        res.send({
+          message: "Sign up successfull",
+          status: "Ok",
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      res.send({
+        ERR: "Something went wrong in signup the data . please try again later",
+      });
+    }
+  }
+});
+//login her
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await Authmodel.find({ email });
+    if (user.length > 0) {
+      const hashed_password = user[0].password;
+      bcrypt.compare(password, hashed_password, function (err, result) {
+        if (result) {
+          const token = jwt.sign({ userID: user[0]._id }, "hush");
+          res.send({ msg: "Login successfull", token: token });
+        } else {
+          res.send("Login failed");
+        }
+      });
+    }
+  } catch {
+    res.send("Something went wrong, please try again later");
+  }
+});
 app.use("/leads", dataRouter);
 
 //running port
@@ -91,7 +87,7 @@ app.listen(PORT, async () => {
     // console.log(process.env.name);
     console.log(`listening on  ${PORT}`);
   } catch (err) {
-    console.log("error while connectoing to db");
+    console.log("error while connectoing to DB");
     console.log(err);
   }
 });
